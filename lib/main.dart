860855585f08d393c875cfa800f07aeb06cb22ed
arthamevia <yuli45904@gmail.com>
2023-02-21 11:01:41 +0700

    Column Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:example/column_widget.dart';
+import 'package:example/row_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,17 +19,47 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           title: Text("Latihan"),
         ),
-        body: Center(
-          child: Text(
-            "Hallo Dunia",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
-            backgroundColor: Colors.black,
-          ),
-          ),
+        body: Column(
+          children: [
+            RowWidget(),
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+            ),
+            ColumnWidget(),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class HelloWidget extends StatelessWidget {
+  const HelloWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WidgetPertama();
+  }
+}
+
+class WidgetPertama extends StatelessWidget {
+  const WidgetPertama({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Hallo Dunia",
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: Colors.blue,
+        backgroundColor: Colors.black,
+      ),
       ),
     );
   }
